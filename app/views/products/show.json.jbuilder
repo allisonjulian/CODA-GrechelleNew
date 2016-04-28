3bd0c242d4578f9471880720d40a1890820be58b
allisonjulian <maryallisonsjulian@Allisons-MacBook-Pro-2.local>
2016-04-28 09:17:09 +0800

    first commit

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :description, :classification, :kind, :size, :remarks, :created_at, :updated_at
---
+++ db/migrate/20160426073954_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :classification
+      t.string :kind
+      t.string :size
+      t.text :remarks
+
+      t.timestamps null: false
+    end
+  end
+end
